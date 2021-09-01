0bbb0b81d67c9d7b2453c5d15237f7004c778bd6
GKenar <kenar.gp@gmail.com>
2021-09-01 22:27:04 +0300

    Добавлен класс MyTableRow

---
+++ XmlFormatter/Common/MyTableRow.cs
@@ -0,0 +1,9 @@
+﻿namespace XmlFormatter.Common
+{
+    class MyTableRow
+    {
+        public string FirstColumn { get; set; }
+        public string SecondColumn { get; set; }
+        public string ThirdColumn { get; set; }
+    }
+}
--- XmlFormatter/MainWindow.xaml.cs
+++ XmlFormatter/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using XmlFormatter.Common;
 
 namespace XmlFormatter
 {
@@ -7,9 +10,23 @@ namespace XmlFormatter
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        List<MyTableRow> list = new List<MyTableRow>
+        {
+            new MyTableRow { FirstColumn = "a", SecondColumn = "b", ThirdColumn = "c" },
+            new MyTableRow { FirstColumn = "d", SecondColumn = "e", ThirdColumn = "f" },
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            dataGrid1.ItemsSource = list;
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine();
         }
     }
 }
