1cc82e81df19677db988be8b27af07bb58a534da
GKenar <kenar.gp@gmail.com>
2021-09-07 12:47:17 +0300

    Добавлены недостающие файлы проекта

---
+++ XmlFormatter/Model/ContentEncoder.cs
@@ -0,0 +1,13 @@
+﻿namespace XmlFormatter.Model
+{
+    static class ContentEncoder
+    {
+        public static string ToBase64(byte[] bytesArray)
+        {
+            if (bytesArray == null)
+                return null;
+
+            return System.Convert.ToBase64String(bytesArray);
+        }
+    }
+}
---
+++ XmlFormatter/Model/FilesLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace XmlFormatter.Model
+{
+    class FilesLoader : IFilesLoader
+    {
+        public List<byte[]> Load(List<string> paths)
+        {
+            var filesContent = new List<byte[]>();
+
+            foreach (var path in paths)
+            {
+                try
+                {
+                    filesContent.Add(File.ReadAllBytes(path));
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+
+            return filesContent;
+        } 
+    }
+}
